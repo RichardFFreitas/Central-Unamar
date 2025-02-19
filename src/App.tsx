aa5b03a19acc5986f508559521c341ecc9364f46
Richard <richardfelipelives@gmail.com>
2025-02-19 15:49:33 -0300

    style: Melhorando o Layout da pagina principal

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@/contexts/AuthContext";
 import Index from "./pages/Index";
 import BusinessRegistration from "./pages/BusinessRegistration";
 import Businesses from "./pages/Businesses";
@@ -11,26 +12,32 @@ import BusinessProfile from "./pages/BusinessProfile";
 import News from "./pages/News";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import TestSupabase from "./components/TestSupabase";
+import PlansPage from "./pages/PlansPage";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
+    <AuthProvider>
     <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/test-supabase" element={<TestSupabase />} />
           <Route path="/register" element={<BusinessRegistration />} />
           <Route path="/businesses" element={<Businesses />} />
           <Route path="/business/:id" element={<BusinessProfile />} />
           <Route path="/news" element={<News />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/plans" element={<PlansPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
+    </AuthProvider>
   </QueryClientProvider>
 );
 
