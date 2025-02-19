85d2a7d2d7cddfef2a3b3737d045c9e8b4b834d7
Richard <richardfelipelives@gmail.com>
2025-02-19 15:57:11 -0300

    feat: adicionando as constantes do filtro

---
+++ src/constantes/categories.ts
@@ -0,0 +1,23 @@
+export const CATEGORIES = [
+    "Todos",
+    "Restaurante",
+    "Hotel",
+    "Pousada",
+    "Mercado",
+    "Farmácia",
+    "Cinema",
+    "Bar",
+    "PetShop",
+    "Veterinario",
+    "Imobiliaria",
+    "Oficina",
+    "Autopeças",
+    "Dentista",
+    "Laboratório",
+    "Clínicas",
+    "Roupas",
+    "Produtos naturais",
+    "Deposito de bebida",
+    "Gráfica",
+    "Outros"
+  ];
---
+++ src/constantes/locations.ts
@@ -0,0 +1,12 @@
+export const LOCATIONS = [
+    "Todos",
+    "Unamar (Tamoios)",
+    "Tamoios",
+    "Aquarius",
+    "Jardim Esperança",
+    "Nova Califórnia (Tamoios)",
+    "Botafogo",
+    "Samburá",
+    "Florestinha",
+    "Long Beach (Tamoios)",
+  ];
