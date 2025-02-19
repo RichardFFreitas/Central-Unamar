caaa635d2b7abe2390af765aeb39af5bb64a935b
Richard <richardfelipelives@gmail.com>
2025-02-19 15:59:52 -0300

    feat: adiciona a pagina de planos

---
+++ src/pages/PlansPage.tsx
@@ -0,0 +1,15 @@
+import { Footer } from "@/components/Footer";
+import Header from "@/components/Header";
+import Plans from "@/components/Plans";
+
+const PlansPage = () => {
+  return (
+    <div>
+        <Header/>
+        <Plans />
+        <Footer/>
+    </div>
+  );
+};
+
+export default PlansPage;
