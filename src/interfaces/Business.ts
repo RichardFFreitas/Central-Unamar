72ba828a298de0a7ed7e427260c94c679451e009
Richard <richardfelipelives@gmail.com>
2025-02-19 15:59:29 -0300

    perf: cria pasta de interface para diminuir repetição de código

---
+++ src/interfaces/Business.ts
@@ -0,0 +1,11 @@
+export interface Business {
+    id: string;
+    name: string;
+    category: string;
+    rating: number;
+    address: string;
+    whatsapp: string;
+    telephone: string;
+    photos: string[];
+    plan: string;
+  }
