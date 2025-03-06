068061edb021868e3219d13536559389029fc17b
Richard <richardfelipelives@gmail.com>
2025-03-06 16:25:55 -0300

    feat: adiciona o grid das notícias na pagina principal

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/Header";
-import SearchBar from "@/components/SearchBar";
+import SearchBar from "@/components/BusinessSearchBar";
 import BusinessCard from "@/components/BusinessCard";
 import FeaturedCarousel from "@/components/FeaturedCarousel";
 import { Footer } from "@/components/Footer";
@@ -7,39 +7,39 @@ import { useSupabase } from "@/hooks/useSupabase";
 import { useEffect, useState } from "react";
 import { Sidebar } from "@/components/Sidebar";
 import { Business } from "@/interfaces/Business";
-
+import { News } from "@/interfaces/News";
+import NewsCard from "@/components/NewsCard";
+import { format } from "date-fns"
+import {ptBR} from "date-fns/locale"
 
 const Index = () => {
-  const { getBusinesses } = useSupabase();
+  const { getBusinesses, getNews } = useSupabase();
+  const [allNews, setAllnews] = useState<News[]>([]);
   const [featuredBusinesses, setFeaturedBusinesses] = useState<Business[]>([]);
   const [allBusinesses, setAllBusinesses] = useState<Business[]>([]);
+  
 
   useEffect(() => {
-    const loadFeaturedBusinesses = async () => {
-      const data = await getBusinesses();
-      const enterpriseBusinesses = data?.filter(business =>
-        business.plan === 'enterprise'
-      ) || [];
+    const loadData = async () => {
+      const newsData = await getNews();
+      const formattedNewsData = newsData.map((news) => ({
+        ...news,
+        date: format(new Date(news.date), "dd-MM-yyyy", { locale: ptBR }),
+      }));
+      setAllnews(formattedNewsData || []);
+
+      const businessesData = await getBusinesses();
+      setAllBusinesses(businessesData || []);
+
+      const enterpriseBusinesses =
+        businessesData?.filter((business) => business.plan === "enterprise") ||
+        [];
       setFeaturedBusinesses(enterpriseBusinesses);
     };
-    loadFeaturedBusinesses();
+
+    loadData();
   }, []);
-
-
-  useEffect(() => {
-    const loadBusinesses = async () => {
-      const data = await getBusinesses();
-
-      setAllBusinesses(data || []); // Armazena todos os negócios
-
-      const enterpriseBusinesses = data?.filter(
-        (business) => business.plan === "enterprise"
-      ) || [];
-
-      setFeaturedBusinesses(enterpriseBusinesses);
-    };
-    loadBusinesses();
-  }, []);
+  console.log(allNews)
 
   return (
     <div className="min-h-screen bg-gray-50">
@@ -59,12 +59,12 @@ const Index = () => {
             Encontre os Melhores comércios de Unamar!
           </h1>
           <div className="max-w-2xl mx-auto">
-            <SearchBar  />
-            <a href="https://wa.me/5522997586193?text=Ol%C3%A1%2C%20vim%20pela%20central%20unamar%2C%20e%20gostaria%20de%20saber%20como%20posso%20anunciar%20tamb%C3%A9m" >
+            <SearchBar />
+            <a href="https://wa.me/5522997586193?text=Ol%C3%A1%2C%20vim%20pela%20central%20unamar%2C%20e%20gostaria%20de%20saber%20como%20posso%20anunciar%20tamb%C3%A9m">
               <button className="mt-8 bg-red-600 text-white px-8 py-3 rounded font-semibold hover:bg-red-700 transition-colors">
                 Fale com um de nossos atendentes.
               </button>
-            </a >
+            </a>
           </div>
         </div>
       </div>
@@ -73,11 +73,15 @@ const Index = () => {
       <section className="mx-auto px-4 py-4 sm:px-6 lg:px-8 xl:mr-80 ">
         {featuredBusinesses.length > 0 && (
           <>
-            <h2 className="text-3xl font-bold text-gray-900 mb-8">Comércios em destaque</h2>
+            <h2 className="text-3xl font-bold text-gray-900 mb-8 text-center">
+              Comércios em destaque
+            </h2>
             <FeaturedCarousel businesses={featuredBusinesses} />
           </>
         )}
-        <h2 className="text-3xl font-bold text-gray-900 mb-8 mt-8">Todos os comércios</h2>
+        <h2 className="text-3xl font-bold text-gray-900 mb-8 mt-8 text-center">
+          Todos os comércios
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
           {allBusinesses.map((business) => (
             <BusinessCard
@@ -91,6 +95,23 @@ const Index = () => {
               whatsapp={business.whatsapp}
               telephone={business.telephone}
               plan={business.plan}
+            />
+          ))}
+        </div>
+        
+        <h2 className="text-3xl font-bold text-gray-900 mb-8 mt-8 text-center">
+          Principais notícias da região
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
+          {allNews.map((news) => (
+            <NewsCard
+              key={news.id}
+              id={news.id.toString()}
+              title={news.title}
+              date={news.date}
+              category={news.category}
+              image={news.image}
+              excerpt={news.excerpt}
             />
           ))}
         </div>
